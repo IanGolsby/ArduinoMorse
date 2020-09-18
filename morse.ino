7bea964dcdeeff2ed42b6abff07508bdb34f7612
Ian Golsby <iangolsby49@gmail.com>
2020-09-17 19:47:21 -0500

    Added comments out of compassion for people who don't want to try and figure out the mess that is my code

--- morse.ino
+++ morse.ino
@@ -1,13 +1,13 @@
-const int pin = 13;
+const int pin = 13; // This pin also corresponds to the builtin led on the board, so this can be run with no external circuitry.
 const int unit = 100; // Time unit length in milliseconds. 100 is somewhat readable if you kinda know some morse but are untrained, and 60 is close to the standard as far as I can tell.
-const int dotLength = unit;
+const int dotLength = unit;     // Lines 3-7 just define the timing standard for morse code.
 const int bitDist = unit;
 const int dashLength = 3*unit;
 const int letterDist = 3*unit;
 const int wordDist = 4*unit;
 String strng = "Put whatever you want here, it'll handle basic punctuation, spacing, and is case-insensitive. Also, if there's something it doesn't know the code for, it ignores it.";
 
-void dot(int pin, boolean final=false) {
+void dot(int pin, boolean final=false) { // This code activates the LED for the length of time required for a dot, then turns it off for the length of time in between signals. If the final variable is true, it's the last signal in the letter and will have a larger downtime gap.
   digitalWrite(pin,1);
   delay(dotLength);
   digitalWrite(pin,0);
@@ -18,7 +18,7 @@ void dot(int pin, boolean final=false) {
   }
 }
 
-void dash(int pin, boolean final=false) {
+void dash(int pin, boolean final=false) { // It's the dot function but the light is on longer
   digitalWrite(pin,1);
   delay(dashLength);
   digitalWrite(pin,0);
@@ -29,10 +29,10 @@ void dash(int pin, boolean final=false) {
   }
 }
 
-void morse(char c) {
+void morse(char c) { // Pass it a character and it will call the functions corresponding to the character in morse code
   if (c=='a') {
     dot(pin);
-    dash(pin,true);
+    dash(pin,true); // Note that the last call for each character always activates the 'final' variable
   } else if (c=='b') {
     dash(pin);
     dot(pin);
@@ -227,28 +227,26 @@ void morse(char c) {
     dash(pin);
     dot(pin,true);
   }
-}
+}       // Yes, I know that I could have done this as like some sort of external file using a single dict variable or whatever Arduino uses but I did this while half asleep and I've already typed all this once I'm not gonna do it again just for different formatting.
 
 void setup() {
-  pinMode(pin, OUTPUT);
-  strng.toLowerCase();
-  strng += ' ';
-}
+  pinMode(pin, OUTPUT); // Define output pin
+  strng.toLowerCase();  // Removes capitalization for convenience
+  strng += ' ';         // Adds a space at the end. This is what allows parseSpaces to run the final word. Otherwise, it stops at the last space and doesn't output every given word.
+}                       // Haven't gotten around to testing it but I'm pretty sure this code will break if the string ends in a space when you pass it. Or maybe not, idk.
 
-void parseSpaces(String str) {
-  if (str.indexOf(' ') != -1) {
-    for (int i = 0; i < str.indexOf(' '); i++) {
+void parseSpaces(String str) {                      // Located in a weird space because I was testing with Serial, which was breaking when I tried to use it before calling .begin() even though it wasn't being executed
+  if (str.indexOf(' ') != -1) {                     // indexOf returns -1 if it doesn't find the passed character. This is breaking the string up into different words.
+    for (int i = 0; i < str.indexOf(' '); i++) {    // For every letter up to the first space, flash the morse code.
       morse(str[i]);
     }
-    delay(wordDist);
-    str.remove(0,str.indexOf(' ')+1);
-    if (str != ' ' && str) {
-      parseSpaces(str);
-    }
+    delay(wordDist);                                // Wait before doing the next word
+    str.remove(0,str.indexOf(' ')+1);               // Cut the word we just did out of the original string.
+    parseSpaces(str);                               // That's right, it's a recursive function. We're just doing it all again without the first word.
   }
 }
 
-void loop() {
+void loop() {           // Just gonna do the thing then wait 5 seconds before doing it again.
   parseSpaces(strng);
   delay(5000);
 }
